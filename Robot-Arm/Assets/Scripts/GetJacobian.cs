41a7094db7f339a7df0554512cc0b0949c005dd7
BenceV <vbence1234@gmail.com>
2018-12-20 21:49:48 +0000

    Arm setup

---
+++ Robot-Arm/Assets/Scripts/GetJacobian.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetJacobian : MonoBehaviour
+{
+    //Simpler
+    public GameObject j0;
+    public GameObject j1;
+    public GameObject j2;
+    public GameObject ee;
+
+    public GameObject l0;
+    public GameObject l1;
+    public GameObject l2;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Quaternion.Angle(l0.transform.rotation, l1.transform.rotation));
+        print(Quaternion.Angle(l1.transform.rotation, l2.transform.rotation));
+        print(Quaternion.Angle(l2.transform.rotation, ee.transform.rotation));
+    }
+}
---
+++ Robot-Arm/Assets/Scripts/SetConfig.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SetConfig : MonoBehaviour {
+    //Not finished Do it later
+    private ArrayList links;
+    private int nChild = 0;
+    //Simpler
+    public GameObject l0;
+    public GameObject l1;
+    public GameObject l2;
+    public float[] rotations;
+
+
+
+    // Use this for initialization
+    void Start () {
+        //Working implementation starts here
+        l0.gameObject.transform.Rotate(new Vector3(1, 0, 0), rotations[0]);
+        l1.gameObject.transform.Rotate(new Vector3(1, 0, 0), rotations[1]);
+        l2.gameObject.transform.Rotate(new Vector3(0, 0, 1), rotations[2]);
+
+
+        //Working implementation ends here
+    }
+    void Update()
+    {
+
+    }
+
+
+    void SetupLinks(GameObject currentParent) {
+        //ISSUE: Not the first child is the correct one.
+        //Add currentParent to the list of links
+        links.Add(currentParent);
+        nChild = currentParent.transform.childCount;
+        if (nChild != 0)
+        {
+            //If the link has a child
+            GameObject nChild = currentParent.transform.GetChild(0).gameObject;
+
+            //Recurse
+            SetupLinks(nChild);
+        }
+        else {
+            //If the link doesn't have a child
+        }
+
+    }
+}
